fd2c19217a64a026efb22436868c00a5f21dab0b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-22 10:05:55 +0200

    changes

--- TCPserver.py
+++ TCPserver.py
@@ -5,3 +5,18 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(("192.168.56.1",8080))
 
 s.listen()
+
+conn,addr=s.accept()
+
+print("Connection received from"+addr[0])
+
+while True:
+    command=input("CMD> ")
+    
+    if command=="exit":
+        conn.send("exit".encode("UTF-8"))
+        conn.close()
+        break
+    
+    conn.send(command.encode("UTF-8"))
+    print(conn.recv(4096).decode("UTF-8"))
